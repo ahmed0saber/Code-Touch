98731c268d411de09d871130703fe3e1b80b2fa9
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-28 18:33:49 +0200

    responsive sidebar

--- sidebars.js
+++ sidebars.js
@@ -6,3 +6,11 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
